bc5bbda02d1026eb42ba6f5ae73efda96f568a77
almant12 <dokoalmant123@gmail.com>
2025-07-13 20:38:21 +0200

    add extractAttr for extract html elements

--- app/Service/ProductScraperService.php
+++ app/Service/ProductScraperService.php
@@ -8,52 +8,35 @@ use Symfony\Component\DomCrawler\Crawler;
 class ProductScraperService
 {
 
-   public function scrapeProduct($url): array
+    public function scrapeProduct($url): array
     {
         $response = Http::get($url);
         $html = $response->getBody();
         $crawler = new Crawler($html);
 
         return [
-            'product_name' => $crawler->filter('h2[property="food:name"]') || null;
-                ? $crawler->filter('h2[property="food:name"]')->text()
-                : 'N/A',
-
-            'brand' => $crawler->filter('#field_brands_value')->count()
-                ? $crawler->filter('#field_brands_value')->text()
-                : 'N/A',
-
-            'categories' => $crawler->filter('#field_categories_value')->count()
-                ? $crawler->filter('#field_categories_value')->text()
-                : 'N/A',
-
-            'labels' => $crawler->filter('#field_labels_value')->count()
-                ? $crawler->filter('#field_labels_value')->text()
-                : 'N/A',
-
-            'countries_sold' => $crawler->filter('#field_countries_value')->count()
-                ? $crawler->filter('#field_countries_value')->text()
-                : 'N/A',
-
-            'barcode' => $crawler->filter('#barcode')->count()
-                ? $crawler->filter('#barcode')->text()
-                : 'N/A',
-
-            'image_url' => $crawler->filter('meta[property="og:image"]')->count()
-                ? $crawler->filter('meta[property="og:image"]')->attr('content')
-                : null,
-
-            'source_url' => $url,
+            'product_name'   => $this->extractText($crawler, 'h2[property="food:name"]'),
+            'brand'          => $this->extractText($crawler, '#field_brands_value'),
+            'categories'     => $this->extractText($crawler, '#field_categories_value'),
+            'labels'         => $this->extractText($crawler, '#field_labels_value'),
+            'countries_sold' => $this->extractText($crawler, '#field_countries_value'),
+            'barcode'        => $this->extractText($crawler, '#barcode'),
+            'image_url'      => $this->extractAttr($crawler, 'meta[property="og:image"]', 'content'),
+            'source_url'     => $url,
         ];
     }
 
 
-    private function extractText(Crawler $crawler, string $selector): ?string{
+    private function extractText(Crawler $crawler, string $selector): ?string
+    {
 
         return $crawler->filter($selector)->count() ?
-        $crawler->filter($selector)->text() : null;
+            $crawler->filter($selector)->text() : null;
     }
-    
+
+    private function extractAttr(Crawler $crawler, string $selector, string $attr): ?string
+    {
+        return $crawler->filter($selector)->count() ?
+            $crawler->filter($selector)->attr($attr) : null;
+    }
 }
-
-
