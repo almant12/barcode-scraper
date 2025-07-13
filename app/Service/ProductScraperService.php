d27397223ccc126b7565f98881f55546c1c933de
almant12 <dokoalmant123@gmail.com>
2025-07-13 21:15:34 +0200

    extract nutrition facts table

--- app/Service/ProductScraperService.php
+++ app/Service/ProductScraperService.php
@@ -22,6 +22,11 @@ class ProductScraperService
             'countries_sold' => $this->extractText($crawler, '#field_countries_value'),
             'barcode'        => $this->extractText($crawler, '#barcode'),
             'image_url'      => $this->extractAttr($crawler, 'meta[property="og:image"]', 'content'),
+            'nutrient_levels' => $this->extractListText($crawler, '#panel_nutrient_levels_content .accordion-navigation h4'),
+            'nutrient_table'=> $this->extractTable($crawler, '#panel_nutrition_facts_table_content table tbody tr'),
+            'ingredients'    => $this->extractText($crawler, '#panel_ingredients_content .panel_text'),
+            'ingredientsInfo' => $this->extractListText($crawler, '#panel_ingredients_list .panel_title h4'),
+
             'source_url'     => $url,
         ];
     }
@@ -38,5 +43,29 @@ class ProductScraperService
     {
         return $crawler->filter($selector)->count() ?
             $crawler->filter($selector)->attr($attr) : null;
+    }
+
+    private function extractListText(Crawler $crawler, string $selector): array
+    {
+        return $crawler->filter($selector)->each(function (Crawler $node) {
+            return trim($node->text());
+        });
+    }
+
+    private function extractTable(Crawler $crawler, string $selector): array {
+        $rows = $crawler->filter($selector);
+
+        return $rows->each(function (Crawler $row){
+            $columns = $row->filter('td');
+            if($columns->count() < 2){
+                return null;
+            }
+
+            return [
+                'name' => trim($columns->eq(0)->text()),
+                'value_per_100g' => trim($columns->eq(1)->text()),
+                'comparison' => trim($columns->eq(2)->text())
+            ];
+        });
     }
 }
